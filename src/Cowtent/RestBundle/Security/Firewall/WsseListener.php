c4dbf155341705715051c91a6606f28df436b88e
Sébastien MALOT <smalot@actualys.com>
2015-08-23 16:21:59 +0200

    code review

--- src/Cowtent/RestBundle/Security/Firewall/WsseListener.php
+++ src/Cowtent/RestBundle/Security/Firewall/WsseListener.php
@@ -7,6 +7,7 @@ use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
@@ -47,14 +48,7 @@ class WsseListener implements ListenerInterface
 
         $wsseRegex = '/UsernameToken Username="([^"]+)", PasswordDigest="([^"]+)", Nonce="([^"]+)", Created="([^"]+)"/';
         if (!$request->headers->has('x-wsse') || 1 !== preg_match($wsseRegex, $request->headers->get('x-wsse'), $matches)) {
-            throw new AccessDeniedHttpException('Missing WSSE headers.');
-
-            $response = new Response();
-            $response->setStatusCode(Response::HTTP_FORBIDDEN);
-            $response->setContent('Missing WSSE headers.');
-            $event->setResponse($response);
-
-            return;
+            throw new BadRequestHttpException('Missing WSSE headers.');
         }
 
         $token = new WsseUserToken();
@@ -67,36 +61,11 @@ class WsseListener implements ListenerInterface
         try {
             $authToken = $this->authenticationManager->authenticate($token);
             $this->tokenStorage->setToken($authToken);
-
-            return;
         } catch (AuthenticationException $failed) {
-            throw new AccessDeniedHttpException('WSSE Login failed.');
-
-            // ... you might log something here
-            $failedMessage = 'WSSE Login failed for '.$token->getUsername().'. Why ? '.$failed->getMessage();
+            $failedMessage = 'WSSE Login failed. Why ? ' . $failed->getMessage();
             $this->logger->err($failedMessage);
 
-            // To deny the authentication clear the token. This will redirect to the login page.
-            // Make sure to only clear your token, not those of other authentication listeners.
-            // $token = $this->tokenStorage->getToken();
-            // if ($token instanceof WsseUserToken && $this->providerKey === $token->getProviderKey()) {
-            //     $this->tokenStorage->setToken(null);
-            // }
-            // return;
-
-            // Deny authentication with a '403 Forbidden' HTTP response
-            $response = new Response();
-            $response->setStatusCode(Response::HTTP_FORBIDDEN);
-            $response->setContent($failedMessage);
-            $event->setResponse($response);
-
-
-            return;
+            throw new AccessDeniedHttpException('WSSE Login failed.');
         }
-
-        // By default deny authorization
-        $response = new Response();
-        $response->setStatusCode(Response::HTTP_FORBIDDEN);
-        $event->setResponse($response);
     }
 }
